8adf820475ae88c7fccdc7aa425cc10e2f270f3c
강인화 <rkddlsghk1998@gmail.com>
2025-01-14 04:49:50 +0900

    feat: 컴포넌트 스캔 테스트

--- src/main/java/hello/core/member/MemoryMemberRepository.java
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -3,9 +3,12 @@ package hello.core.member;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class MemoryMemberRepository implements MemberRepository {
 
-    private static Map<Long, Member> store = new HashMap<Long, Member>();
+    private static final Map<Long, Member> store = new HashMap<>();
 
     @Override
     public void save(Member member) {
