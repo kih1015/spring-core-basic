7dccfcee44a12f10c2ecd782310b295061f756e3
강인화 <rkddlsghk1998@gmail.com>
2025-01-14 07:40:53 +0900

    feat: 웹 스코프와 프록시

--- src/main/java/hello/core/logdemo/LogDemoService.java
+++ src/main/java/hello/core/logdemo/LogDemoService.java
@@ -1,7 +1,6 @@
 
 package hello.core.logdemo;
 
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Service;
 
 import hello.core.common.MyLogger;
@@ -10,10 +9,10 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 public class LogDemoService {
-    private final ObjectProvider<MyLogger> myLoggerProvider;
+
+    private final MyLogger myLogger;
 
     public void logic(String id) {
-        MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.log("service id = " + id);
     }
 }
